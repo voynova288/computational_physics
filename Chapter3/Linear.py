b4b4826ddf96b8b53133e988d252624ec3b7938b
voynova288 <zzy288@163.com>
2023-10-18 15:58:15 +0800

    Update Linear.py

--- Chapter3/Linear.py
+++ Chapter3/Linear.py
@@ -225,3 +225,65 @@ def LU_Solve(A, b):
 
         return Particular_Sol, General_Sol
 
+
+# *雅可比法求矩阵的特征值
+def Jocabi_Eigen(A, err=None):
+    A = np.array(A)
+    row_A, col_A = A.shape
+    if row_A != col_A:
+        print("Jacobi_Eigen Error: Matrix is not square. There is no eigenvalue")
+        return None
+    elif not np.array_equal(A, A.T):
+        print(
+            "Jacobi_Eigen Warning: Matrix is not symmetric. The method can not be used. Anathor mathod is used to generate teh enginvalue"
+        )
+
+    if err is None:
+        err = np.min(np.abs(A)) * 0.01
+
+    n = 0
+    B = np.copy(A)
+    np.fill_diagonal(B, 0)
+    Max_B = np.max(np.abs(B))
+
+    while Max_B >= err and n <= 20:
+        Index_Max_B_ = [list(np.where(B == Max_B)[0]), list(np.where(B == Max_B)[1])]
+        Index_Max_B = [
+            [Index_Max_B_[0][i], Index_Max_B_[1][i]]
+            for i in range(len(Index_Max_B_[0]))
+            if Index_Max_B_[0][i] < Index_Max_B_[1][i]
+        ]
+
+        for k in range(len(Index_Max_B)):
+            J = np.identity(row_A)
+            theta = (
+                np.pi / 4
+                if A[Index_Max_B[k][0], Index_Max_B[k][0]]
+                == A[Index_Max_B[k][1], Index_Max_B[k][1]]
+                else 0.5
+                * np.arctan(
+                    (2 * A[Index_Max_B[k][0]][Index_Max_B[k][1]])
+                    / (
+                        A[Index_Max_B[k][0], Index_Max_B[k][0]]
+                        - A[Index_Max_B[k][1], Index_Max_B[k][1]]
+                    )
+                )
+            )
+            J[Index_Max_B[k][0], Index_Max_B[k][0]] = J[
+                Index_Max_B[k][1], Index_Max_B[k][1]
+            ] = np.cos(theta)
+            J[Index_Max_B[k][0], Index_Max_B[k][1]] = np.sin(theta)
+            J[Index_Max_B[k][1], Index_Max_B[k][0]] = -np.sin(theta)
+            A = np.dot(J, np.dot(A, J.T))
+
+            B = np.copy(A)
+            np.fill_diagonal(B, 0)
+            Max_B = np.max(np.abs(B))
+        
+        n = n+1
+    
+    return [A[i][i] for i in range(row_A)]
+
+
+Eigen = Jocabi_Eigen([[3, 2, 5], [2, 2, 9], [5, 9, 1]])
+print(f'{Eigen}')
