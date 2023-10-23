7335220cc05615d3931684606cdde9cd4770c193
voynova288 <zzy288@163.com>
2023-10-23 20:22:06 +0800

    Update Linear.py

--- Chapter3/Linear.py
+++ Chapter3/Linear.py
@@ -226,14 +226,16 @@ def LU_Solve(A, b):
         return Particular_Sol, General_Sol
 
 
-# *雅可比法求矩阵的特征值
-def Jocabi_Eigen(A, err=None):
+# *雅可比法求矩阵的特征值，这种方法只能求对称矩阵的特征值
+# *输入：矩阵A，误差范围（可选，默认为A中最小元素的0.01倍），最大迭代次数(可选，默认20次)
+# *输出：矩阵A的特征值
+def Jocabi_Eigen(A, err=None, step=None):
     A = np.array(A)
     row_A, col_A = A.shape
-    if row_A != col_A:
+    if row_A != col_A:  # 检查是否为方阵
         print("Jacobi_Eigen Error: Matrix is not square. There is no eigenvalue")
         return None
-    elif not np.array_equal(A, A.T):
+    elif not np.array_equal(A, A.T):  # 检查是否为对称矩阵
         print(
             "Jacobi_Eigen Warning: Matrix is not symmetric. The method can not be used. Anathor mathod is used to generate teh enginvalue"
         )
@@ -241,12 +243,15 @@ def Jocabi_Eigen(A, err=None):
     if err is None:
         err = np.min(np.abs(A)) * 0.01
 
+    if step is None:
+        step = 20
+
     n = 0
     B = np.copy(A)
     np.fill_diagonal(B, 0)
     Max_B = np.max(np.abs(B))
 
-    while Max_B >= err and n <= 20:
+    while Max_B >= err and n <= step:
         Index_Max_B_ = [list(np.where(B == Max_B)[0]), list(np.where(B == Max_B)[1])]
         Index_Max_B = [
             [Index_Max_B_[0][i], Index_Max_B_[1][i]]
@@ -285,3 +290,45 @@ def Jocabi_Eigen(A, err=None):
     Eigenvalues = [A[i][i] for i in range(row_A)]
 
     return Eigenvalues
+
+
+# *用Householder方法将矩阵转化为上Hessenberg矩阵
+# *输入：一个矩阵A，从第几个指标开始变换（可选参数，默认为1）
+# *输出：用Householder方法从A得到的上Hessenberg矩阵
+def Householder_Hessenberg(A, Shape=1):
+    A = np.array(A)
+    row_A, col_A = A.shape
+    if row_A != col_A:  # 检查是否为方阵
+        raise ValueError("Matrix is not square")
+
+    for k in range(row_A - Shape):
+        d = np.sqrt(
+            sum(A[k + i, k] * np.conj(A[k + i, k]) for i in range(Shape, row_A - k))
+        )
+        sigma = np.sign(A[k + Shape, k]) * d
+        beta = (sigma + A[k + Shape, k]) * sigma
+        u = np.array(
+            [
+                A[k + i, k] + sigma if i == Shape else A[k + i, k]
+                for i in range(Shape, row_A - k)
+            ]
+        )
+        u_col = u.reshape(len(u), 1)
+        Rk = (
+            np.eye(row_A - k - Shape) - (1 / beta) * np.dot(u_col, u_col.T)
+            if row_A - k - Shape > 1
+            else np.array([[1]]) - (1 / beta) * np.dot(u_col, u_col.T)
+        )
+        print(f'{Rk}')
+        Ik = np.eye(k + Shape)
+        Uk = np.block(
+            [
+                [Ik, np.zeros((Ik.shape[0], Rk.shape[1]))],
+                [np.zeros((Rk.shape[0], Ik.shape[1])), Rk],
+            ]
+        )
+        A = np.dot(Uk, np.dot(A, Uk))
+
+    print(f"{A}")
+    return A
+
